2cc9c88bdac8578a13bb21ff7cf87b636a1a60f4
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-06-28 20:45:41 -0300

    Adding examples of types

---
+++ 01-quick-start/types.js
@@ -0,0 +1,16 @@
+function log(message) {
+    console.log(message);
+}
+log('Hello World TypeScript');
+var test = ['Hello', 'World'];
+var test2 = ['other teste'];
+var isActive = true;
+/**
+ * string
+ * number
+ * boolean
+ * array
+ * function
+ * ...
+ * Main Types (My Class)
+ */ 
---
+++ 01-quick-start/types.ts
@@ -0,0 +1,19 @@
+function log(message: string) {
+    console.log(message);
+}
+
+log('Hello World TypeScript');
+
+let test: Array<string> = ['Hello', 'World'];
+let test2: [string] = ['other teste'];
+let isActive: boolean = true;
+
+/**
+ * string
+ * number
+ * boolean
+ * array
+ * function
+ * ...
+ * Main Types (My Class)
+ */
