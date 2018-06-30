6f14e84f1c0a5ff182ea3b459073ce7546a02729
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-06-30 17:40:36 -0300

    Adding teste for Horse class

--- 01-quick-start/runAnimal.js
+++ 01-quick-start/runAnimal.js
@@ -1,5 +1,8 @@
 "use strict";
 exports.__esModule = true;
 var Animal_1 = require("./Animal");
+var Horse_1 = require("./Horse");
 var animal = new Animal_1.Animal('Dog');
 animal.move(50);
+var horse = new Horse_1.Horse('Flash Horse');
+horse.move(100);
--- 01-quick-start/runAnimal.ts
+++ 01-quick-start/runAnimal.ts
@@ -1,4 +1,8 @@
 import { Animal } from "./Animal";
+import { Horse } from "./Horse";
 
 const animal = new Animal('Dog');
 animal.move(50);
+
+const horse = new Horse('Flash Horse');
+horse.move(100);
