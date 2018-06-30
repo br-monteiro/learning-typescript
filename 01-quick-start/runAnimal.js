1c0a97f3304b00a188cca105505c235a69d25825
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-06-30 17:31:43 -0300

    Adding test of Animal class

---
+++ 01-quick-start/runAnimal.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var Animal_1 = require("./Animal");
+var animal = new Animal_1.Animal('Dog');
+animal.move(50);
---
+++ 01-quick-start/runAnimal.ts
@@ -0,0 +1,4 @@
+import { Animal } from "./Animal";
+
+const animal = new Animal('Dog');
+animal.move(50);
